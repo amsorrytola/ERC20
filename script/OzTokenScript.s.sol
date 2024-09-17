9118fbb6fce0a8abd9669526fba7c50227270c66
amsorrytola <talhaansariitr@gmail.com>
2024-09-18 00:35:00 +0530

    chore: forge init

--- script/OzTokenScript.s.sol
+++ script/OzTokenScript.s.sol
@@ -1,8 +1,8 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {Script} from "forge-std/Script.sol"
-import {OzToken} from "./src/OzToken.sol"
+import {Script} from "forge-std/Script.sol";
+import {OzToken} from "./src/OzToken.sol";
 
 contract OzTokeScript is Script {
     uint256 public constant INITIALSUPPLY = 1000 ether;
--- src/OzToken.sol
+++ src/OzToken.sol
@@ -1,6 +1,6 @@
 //SPDX-License-Identifier: MIT 
 pragma solidity ^0.8.18;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract OzToken is ERC20{
     constructor(uint256 initialSupply) ERC20("TolaToken","TT"){
